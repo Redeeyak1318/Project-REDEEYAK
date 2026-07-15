846545021b45fe97bb7f26fe3840e0995155b38b
Raktim Sonowal <Redeeyak1318@users.noreply.github.com>
2026-07-15 23:45:47 +0530

    feat(hero): compose hero section layout

--- frontend/components/hero/Hero.tsx
+++ frontend/components/hero/Hero.tsx
@@ -4,34 +4,36 @@ import { HeroImage } from "./HeroImage";
 import { ScrollIndicator } from "./ScrollIndicator";
 import { cn } from "@/lib/utils";
 
-interface HeroProps {
-  className?: string;
-}
+export interface HeroProps extends React.HTMLAttributes<HTMLElement> { }
 
-export const Hero = ({ className }: HeroProps) => {
+export const Hero = ({
+  className,
+  ...props
+}: HeroProps) => {
   return (
-    <section 
+    <section
       id="home"
+      {...props}
       className={cn(
-        "relative min-h-[calc(100vh-4rem)] flex flex-col justify-center pt-16 pb-24 lg:pt-24 lg:pb-32 overflow-hidden", 
+        "relative min-h-[calc(100vh-4rem)] flex flex-col justify-center pt-16 pb-24 lg:pt-24 lg:pb-32",
         className
       )}
       aria-label="Introduction"
     >
-      <Container className="relative z-10 flex-grow flex flex-col justify-center">
-        <div className="grid grid-cols-1 lg:grid-cols-12 gap-12 lg:gap-8 items-center">
-          <div className="lg:col-span-7 xl:col-span-8 order-2 lg:order-1">
+      <Container className="flex-grow flex flex-col justify-center">
+        <div className="grid grid-cols-1 lg:grid-cols-2 gap-12 lg:gap-8 items-center">
+          <div className="order-2 lg:order-1">
             <HeroContent />
           </div>
-          <div className="lg:col-span-5 xl:col-span-4 order-1 lg:order-2">
+          <div className="order-1 lg:order-2">
             <HeroImage />
           </div>
         </div>
+        <div className="mt-16 hidden md:flex justify-center">
+          <ScrollIndicator />
+        </div>
       </Container>
-      
-      <div className="absolute bottom-8 left-1/2 -translate-x-1/2 hidden md:block">
-        <ScrollIndicator />
-      </div>
+
     </section>
   );
 };
