d73eb3b021cea7391a86d50ba226f6a105a84b62
Raktim Sonowal <Redeeyak1318@users.noreply.github.com>
2026-07-16 17:35:54 +0530

    feat(about): compose about section layout

--- frontend/components/about/About.tsx
+++ frontend/components/about/About.tsx
@@ -1,5 +1,11 @@
+import * as React from "react";
+
 import { Container } from "@/components/layout";
 import { cn } from "@/lib/utils";
+
+import { AboutContent } from "./AboutContent";
+import { AboutImage } from "./AboutImage";
+import { AboutStats } from "./AboutStats";
 
 export interface AboutProps extends React.HTMLAttributes<HTMLElement> { }
 
@@ -7,10 +13,20 @@ export const About = ({ className, ...props }: AboutProps) => {
   return (
     <section
       id="about"
-      className={cn(className)}
+      className={cn(
+        "py-20 lg:py-28",
+        className
+      )}
       {...props}
     >
       <Container>
+        <div className="grid grid-cols-1 lg:grid-cols-2 gap-12 lg:gap-16 items-center">
+          <AboutContent />
+
+          <AboutImage />
+        </div>
+
+        <AboutStats className="mt-12 lg:mt-16" />
       </Container>
     </section>
   );
